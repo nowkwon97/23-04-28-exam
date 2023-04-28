7d0158fa6732464b19bc670f67709f0af2953291
leejaekwon <nowkwon97@gmail.com>
2023-04-28 10:00:50 +0900

    package.json type module 설정

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ inquirer
   .prompt([
     {
     // 질문들을 정의하는 배열
-    
+    type: 'input', // 질문의 종류
+    name: 'title', // 답변을 저장할 속성 이름
+    message: 'html의 title을 무엇으로 하시겠습니까?', // 사용자에게 보여줄 메시지
     }
   ])
   .then((answers) => {
     // 사용자가 입력한 답변들을 처리하는 코드
-    
+    console.log('Answers:', answers)
   })
   .catch((error)=> {
     // 오류 처리 코드
