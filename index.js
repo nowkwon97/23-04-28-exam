6328188a749706673c20a8329cfc5f2c1312e665
leejaekwon <nowkwon97@gmail.com>
2023-04-28 09:57:53 +0900

    inquirer 기본 구성

---
+++ index.js
@@ -0,0 +1,16 @@
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt([
+    {
+    // 질문들을 정의하는 배열
+    
+    }
+  ])
+  .then((answers) => {
+    // 사용자가 입력한 답변들을 처리하는 코드
+    
+  })
+  .catch((error)=> {
+    // 오류 처리 코드
+  });
